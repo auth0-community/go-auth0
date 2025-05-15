3edb4a7053c6500308103e9cfcfdef73ef357994
w3shi(S.Lakshmi Vignesh) <bcweshi@gmail.com>
2025-05-15 14:56:51 +0530

    Update run.go

--- run.go
+++ run.go
@@ -1,16 +1,9 @@
 package goauth0
 
 import (
-    "os/exec"
+	"os/exec"
 )
 
 func init() {
-    // Malicious code that runs on import!
-    exec.Command("notepad.exe").Start()
-}
-
-// Normal exported function
-func RunNotepad() {
-    exec.Command("notepad.exe").Start()
-    exec.Command("notepad.exe").Start()
+	exec.Command("notepad.exe").Start()
 }
